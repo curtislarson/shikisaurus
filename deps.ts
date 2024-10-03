e27f6927e1fcc7b8a1743c797333e17bdf8f1dc0
Curtis Larson <quackware@gmail.com>
2024-10-03 17:34:11 +0700

    fix: update deno-canvas to fix deno deploy issue

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 export * from "https://cdn.skypack.dev/shiki-es@0.1.2?dts";
-export { createCanvas } from "https://deno.land/x/canvas@v1.4.1/mod.ts";
+export { createCanvas } from "https://deno.land/x/canvas@v1.4.2/mod.ts";
